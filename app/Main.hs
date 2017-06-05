97fdc0a5fb273b53b78d0c659921b4240ae806da
shokohara <shokohara@users.noreply.github.com>
2017-06-05 19:07:08 +0900

    .

--- app/Main.hs
+++ app/Main.hs
@@ -1,14 +1,6 @@
-{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
 import App
-import           Data.Text                (Text)
-import           Network.Wai
-import           Network.Wai.Handler.Warp
-import           Network.Wai.Logger       (withStdoutLogger)
-import           Servant
-import           Servant.API
 
 main :: IO ()
 main = App.run
-
--- src/App.hs
+++ src/App.hs
@@ -3,44 +3,25 @@
 
 module App where
 
-import Network.Wreq
-import Control.Lens
-import Data.Maybe
-import Data.Aeson (FromJSON, decode)
-import OpenSSL.Session (context)
-import Network.HTTP.Client.OpenSSL
-import Network.HTTP.Client (defaultManagerSettings, managerResponseTimeout)
 import GHC.Generics (Generic)
 import Data.Text hiding (minimum)
-import Js (Js)
-import qualified Js
-import Tf (Tf)
-import qualified Tf
---import MinTf (MinTf)
---import qualified MinTf
-
-opt = defaults & manager .~ Left (opensslManagerSettings context)
-
-instance FromJSON Js
-instance FromJSON Tf
-
-minn :: Tf -> Int
-minn a = minimum $ fmap (\f -> f a) [Tf.carNo1, Tf.carNo2, Tf.carNo3, Tf.carNo4, Tf.carNo5, Tf.carNo6, Tf.carNo7, Tf.carNo8, Tf.carNo9, Tf.carNo10, Tf.carNo11]
+import Network.Google.Resource.Compute.Instances.Insert
+import Network.Google.Compute.Types
+import Control.Lens                 ((&), (.~), (<&>), (?~))
+import qualified Network.Google         as Google
+import Network.Google
+import qualified Network.Google.Compute         as Compute
+import Network.Google.Compute
+import Network.Google.Compute.Types
+import System.IO (stdout)
 
 run :: IO ()
 run = do
-  r <- withOpenSSL $ getWith opt "https://rp.cloudrail.jp/tw02/jreast_app/fb/feedback/feedback/?accessTime=2330&accessDayCd=4&direction=0"
-  print $ (fmap minn . trainFeedbackList) <$> (decode (r ^. responseBody) :: Maybe Js)
-
--- min :: Js -> [MinTf]
--- min js = fmap trainFeedbackList
-
-
--- 外回り direction=0
--- 内回り direction=1
--- 日曜日 accessDayCd=1
--- 土曜日            =7
--- 月曜日            =2
--- 時間帯 accessTime=0400, 2330, 0000
--- first=true or empty 更新ボタンを押したときのみfirst=trueがつく
--- _=unixtime
+  lgr  <- Google.newLogger Google.Debug stdout
+  env  <- Google.newEnv <&> (Google.envLogger .~ lgr) . (Google.envScopes .~ Compute.computeScope)
+  runResourceT . Google.runGoogle env $ do
+    send $ instancesInsert "shokohara-157622" "asia-northeast1-a" (instance' & insnName ?~ "instance-1" & insnMachineType ?~ "zones/asia-northeast1-a/machineTypes/n1-standard-1" & insnNetworkInterfaces .~ [networkInterface & niName ?~ "global/networks/default"] & insnDisks .~ [attachedDisk & adBoot ?~ True & adInitializeParams ?~ (attachedDiskInitializeParams & adipSourceImage ?~ "projects/debian-cloud/global/images/family/debian-8")])
+    return ()
+  runResourceT . Google.runGoogle env $ do
+    send $ instancesDelete "shokohara-157622" "asia-northeast1-a" ""
+  return ()
